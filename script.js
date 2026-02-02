58dde2358e8111fb4434be0c06d67740137fe8cf
bjorndalman <bjorn.klas.dahlman@gmail.com>
2026-02-02 18:57:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ const defaultData = { // ENGELSKA (BAS)
       school: "K3 - Karlsborg",
       program: "Military Service - Rangers Training",
       years: "",
-      description: "<img src=\"jagar_badge.jpg\" alt=\"Ranger badge\" style=\"height: 18px; vertical-align: middle; margin-right: 5px;\"> Completed advanced basic military training as a Ranger (Jägare). Experience from the Life Regiment Hussars (K3) in Karlsborg, where operations are characterized by high levels of responsibility, teamwork under pressure, and the use of advanced technology. The unit focuses on intelligence gathering, reconnaissance, and rapid response capabilities in complex environments, as well as training in survival and personnel recovery for international missions."
+      description: "<picture><source srcset=\"images/jagar_badge.webp\" type=\"image/webp\"><img src=\"images/jagar_badge.jpg\" alt=\"Ranger badge\" style=\"height: 18px; vertical-align: middle; margin-right: 5px;\"></picture> Completed advanced basic military training as a Ranger (Jägare). Experience from the Life Regiment Hussars (K3) in Karlsborg..."
     }
-      ],
+    ],
   experience: [
     {
       title: "Employment & Studies",
@@ -305,6 +305,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 });
+
 
 
 
